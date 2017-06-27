1c24237aae5b5c9b0f182b2ffe2a4cf3cfa1353e
Ajiboye Eniola O <eloyjaws@gmail.com>
2017-06-27 23:43:59 +0100

    Implement last callout from mockup, PickupActivity

---
+++ app/src/main/java/com/example/android/resturant/PickupActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.resturant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pickup);
+    }
+}
